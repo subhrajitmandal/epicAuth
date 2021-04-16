57b0e49f0969bdc3bb86ea94a360a85acf924be9
subhrajitmandal <dipudipu2007@gmail.com>
2021-04-16 16:55:35 +0530

    Update index2.js

--- index2.js
+++ index2.js
@@ -1,6 +1,7 @@
 'use strict';
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
+var axios = require('axios');
 var privateKEY  = fs.readFileSync('./privatekey.pem', 'utf8');
 var publicKEY  = fs.readFileSync('./publickey509.pem', 'utf8');
 /*
@@ -12,8 +13,8 @@ const myEpoch = myDate.getTime() / 1000.0;
 const epochTime = Math.floor(myEpoch) + 200;
 
 // console.log(Math.floor(myEpoch))
-console.log(epochTime)
-console.log(myDate)
+// console.log(epochTime)
+// console.log(myDate)
 
 function uuidv4() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
@@ -27,9 +28,9 @@ var payload = {
     "sub": "a41a5b14-476b-4043-8e05-820c16a37395",
     "aud": "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token",
     "jti": uuidv4(),
-    "exp": epochTime,
-    "nbf": epochTime,
-    "iat": epochTime
+    "exp": epochTime
+    // "nbf": 1617933871,
+    // "iat": 1617933871
 };
 var i  = 'HCL Technologies';   
 var s  = 'subhrajitm@hcl.com';   
@@ -55,3 +56,33 @@ var verifyOptions = {
 };
 var legit = jwt.verify(token, publicKEY, verifyOptions);
 console.log("\nJWT verification result: " + JSON.stringify(legit));
+
+// axios
+//   .post('https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token', {
+//     grant_type: 'client_credentials',
+//     client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+//     client_assertion: token
+//   })
+//   .then(res => {
+//     console.log(`statusCode: ${res.statusCode}`)
+//     console.log(res)
+//   })
+//   .catch(error => {
+//     console.error(error)
+//   })
+
+const axios = require('axios');
+
+// Make a request for a user with a given ID
+axios.get('/user?ID=12345')
+  .then(function (response) {
+    // handle success
+    console.log(response);
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .then(function () {
+    // always executed
+  });
